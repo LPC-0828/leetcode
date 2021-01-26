58fd100ad51624ca266310fc43db6150118acf6b
雷鹏程 <root>
2021-01-26 14:51:23 +0800

    master

--- src/main/java/com/rt/demo/api/DemoApi.java
+++ src/main/java/com/rt/demo/api/DemoApi.java
@@ -1,7 +1,9 @@
 package com.rt.demo.api;
 
+import com.rt.demo.service.DemoService;
 import com.rt.model.Response;
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApi {
 
+    @Autowired
+    DemoService demoService;
     @GetMapping("/")
     public Response click() {
-
+        demoService.insert();
         return Response.success("成功");
     }
 }
---
+++ src/main/java/com/rt/demo/service/DemoService.java
@@ -0,0 +1,11 @@
+package com.rt.demo.service;
+
+/**
+ * @ClassName DemoService
+ * @Author Ray
+ * @Date 2021/1/26 14:41
+ * @Description
+ */
+public interface DemoService {
+    Integer insert();
+}
