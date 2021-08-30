0b28b32cb28312f2b561e941da52e4e3239c12ed
雷鹏程 <54sb54sb>
2021-08-30 21:28:18 +0800

    1224

--- src/main/java/com/leetcode/demo/lpc/最大相等频率.java
+++ src/main/java/com/leetcode/demo/lpc/最大相等频率.java
@@ -1,6 +1,7 @@
 package com.leetcode.demo.lpc;
 
 import org.junit.Test;
+import java.util.HashMap;
 
 /**
  * <p>
@@ -11,14 +12,22 @@ import org.junit.Test;
  * 输入：nums = [2,2,1,1,5,3,3,5]
  * 输出：7
  * 解释：对于长度为 7 的子数组 [2,2,1,1,5,3,3]，如果我们从中删去 nums[4]=5，就可以得到 [2,2,1,1,3,3]，里面每个数字都出现了两次。
+ *
  * @author Ray
  * @date: 2021.08.30 09:52
  */
 public class 最大相等频率 {
 
     @Test
-    public void 自己(int[] nums) {
+    public void 自己() {
+        int[] nums = new int[]{10, 2, 8, 9, 3, 8, 1, 5, 2, 3, 7, 6};
+        for (int i = 0; i < nums.length; i++) {
+            // i 是去掉的元素 去掉之后 重新进行排列
+            // 1.记录取掉元素之后每个频率最长的长度 循环结束之后 取最大
+            int num = nums[i];
+            HashMap<Integer, Integer> temp = new HashMap<>();
 
+        }
     }
 
 }
