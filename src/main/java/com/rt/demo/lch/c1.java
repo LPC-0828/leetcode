b7305e98618ced3a3d1a382e30967d3d42b57c54
雷鹏程 <54sb54sb>
2021-08-30 15:53:29 +0800

    test

---
+++ src/main/java/com/rt/demo/lch/c1.java
@@ -0,0 +1,19 @@
+package com.rt.demo.lch;
+
+import org.junit.Test;
+
+/**
+ * <p>
+ * <p/>
+ *
+ * @author Lch
+ * @date: 2021.08.30 09:51
+ */
+public class c1 {
+
+    @Test
+    public void testQuery(){
+        System.out.println(1);
+    }
+
+}
---
+++ src/main/java/com/rt/demo/lpc/c1.java
@@ -0,0 +1,19 @@
+package com.rt.demo.lpc;
+
+import org.junit.Test;
+
+/**
+ * <p>
+ * <p/>
+ *
+ * @author Ray
+ * @date: 2021.08.30 09:52
+ */
+public class c1 {
+
+    @Test
+    public void testQuery(){
+        System.out.println(1);
+    }
+
+}
